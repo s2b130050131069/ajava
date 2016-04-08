0893f489a991ba370b52ef40db3a8ee4227fbefd
s2b130050131069 <kishanmore1150@gmail.com>
2016-04-08 10:42:47 -0700

    Prac4.20

--- Prac4/4.20/ServletFetchingData.java
+++ Prac4/4.20/ServletFetchingData.java
@@ -1,82 +1,91 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.sql.*;
 
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet implementation class ServletFetchingData
- */
-@WebServlet("/ServletFetchingData")
-public class ServletFetchingData extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+public class ServletFetchingData extends HttpServlet{
+	// JDBC driver name and database URL
 	static final String JDBC_DRIVER = "org.postgresql.Driver";
-	static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/s2d130050131528";
+	static final String DB_URL = "jdbc:postgresql://127.0.0.1:5433/s2b130050131069";
 
+	//  Database credentials
 	static final String USER = "postgres";
-	static final String PASS = "12345";
-
-	Connection Conn;
-	Statement stmt;
-	
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		PrintWriter out = response.getWriter();
-		response.setContentType("text/html");
-		ResultSet rs = null;
-		try {
-			Class.forName(JDBC_DRIVER);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	static final String PASS = "kishan1150";
+	   
+	public void init() throws ServletException{
 		
-		
-		try {
-			Conn=DriverManager.getConnection(DB_URL, USER, PASS);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			stmt=Conn.createStatement();
-			String sql="select * from registration";
-			rs=stmt.executeQuery(sql);
-			while (rs.next())
-			{
-				out.println("<h5>"+rs.getInt(1)+"\t\t"+rs.getString(2)+"\t\t"+rs.getString(3)+"<h5>");
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		out.println("Enroll No: 130050131528");
-
-	
 	}
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		Connection conn = null;
+	    Statement stmt = null;
+		try{
+			System.out.println("Enrollno: 130050131069");
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+            //STEP 3: Open a connection
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Connected database successfully...");
+			
+			stmt = conn.createStatement();
+		    
+			//STEP 2: Executing query		
+			String name = "ASDF";
+			String rollno = "20";
+			String branch = "CSE";
+		    String sql = "SELECT * FROM student";
+	
+		    stmt.executeQuery(sql);
+		    
+		    ResultSet rs = stmt.getResultSet();
+        	out.print("| <b>Rollno</b>| ");
+            out.print("<b>Name|</b> ");
+            out.print("<b>Branch</b>| ");
+            out.println("</br>\n-------------------------------</br>");
+            while (rs.next())
+            {
+            	out.println();
+            	out.print("| "+rs.getString(1));
+            	out.print("| "+rs.getString(2));
+                out.print("| "+rs.getString(3)+"|");
+                out.println("</br>");
+           }
+	            
+		}catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		   }catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		   }finally{
+		      //finally block used to close resources
+		      try{
+		         if(stmt!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		      }// do nothing
+		      try{
+		         if(conn!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		         se.printStackTrace();
+		      }//end finally try
+		   }//end try
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
-
+	
+	public void destroy(){
+		
+	}
+  
 }
